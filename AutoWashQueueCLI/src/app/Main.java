6cc3083901dc110ca40d4378413b2d13200bddd0
NgaiLong49423 <163678565+NgaiLong49423@users.noreply.github.com>
2026-06-28 00:01:42 +0700

    feat: implement generic MyLinkedList and add parsing utilities for data input

--- AutoWashQueueCLI/src/app/Main.java
+++ AutoWashQueueCLI/src/app/Main.java
@@ -1,13 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package app;
+
+import datastructure.MyLinkedList;
 
 /**
  *
  * @author Lenovo
  */
 public class Main {
-    
+    public static void main(String[] args) {
+        MyLinkedList<String> list = new MyLinkedList<>();
+
+        list.addLast("A");
+        list.addLast("B");
+        list.addFirst("X");
+
+        list.display();
+
+        System.out.println("First: " + list.getFirst());
+        System.out.println("Last: " + list.getLast());
+        System.out.println("Size: " + list.size());
+    }
 }
